6404a6d4cb9a38487809454adb8ef44c10ce18de
minnnn7716 <minnn7716@gmail.com>
2022-11-06 15:51:34 +0800

    add

---
+++ js/main.js
@@ -0,0 +1,107 @@
+const dataUrl = "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json";
+let data;
+const list = document.querySelector("#resultBlock");
+const filter = document.querySelector("#select");
+const filterResult = document.querySelector("#filterResult");
+const addbtn = document.querySelector(".addbtn");
+const addName = document.querySelector("#name");
+const addImgUrl = document.querySelector("#picUrl");
+const addArea = document.querySelector("#area");
+const addPrice = document.querySelector("#price");
+const addGroup = document.querySelector("#number");
+const addRate = document.querySelector("#star");
+const addDescip = document.querySelector("#descip");
+
+axios.get(dataUrl)
+  .then(function (response) {
+    data = response.data.data;
+    
+    renderData(data);
+    addDate();
+    filterArea();
+  })
+
+// 選擇地區篩選資料
+function filterArea(){
+  filter.addEventListener('change',e => {
+    let filterArea = e.target.value;
+    if(filterArea === "全部"){
+      renderData(data);
+      filterResult.textContent = `共有 ${data.length} 筆資料，歡迎使用地區搜尋`;
+    }else{
+      newData = data.filter(i => i.area === filterArea);
+      renderData(newData);
+      filterResult.textContent = `本次搜尋共 ${newData.length} 筆資料`;
+    }
+  });
+};
+
+// 新增資料
+function addDate(){
+  addbtn.addEventListener('click',e => {
+    let obj = {};
+    
+    if(
+      addName.value.trim() === "" ||
+      addImgUrl.value.trim() === "" ||
+      addArea.value.trim() === "" ||
+      addDescip.value.trim() === "" ||
+      addGroup.value.trim() === "" ||
+      addPrice.value.trim() === "" ||
+      addRate.value.trim() === ""){
+        alert(`欄位不得為空，請填入資訊`);
+    }else{
+      obj.id = data.length;
+      obj.name = addName.value;
+      obj.imgUrl = addImgUrl.value;
+      obj.area = addArea.value;
+      obj.description = addDescip.value;
+      obj.group = + addGroup.value;
+      obj.price = + addPrice.value;
+      obj.rate = + addRate.value;
+      data.push(obj);
+  
+      renderData(data);
+      document.forms["addForm"].reset();
+    }
+  
+  })
+}
+
+// 選染資料
+function renderData(data){
+  let content = "";
+
+  data.forEach(i => {
+    content += `
+    <a class="col-4 mb-5" href="#">
+      <div class="card">
+        <div class="cardHeader">
+          <span class="aeraTag">${i.area}</span>
+          <span class="scoreTag">${i.rate}</span>
+          <div class="cardImg"><img src="${i.imgUrl}" alt="${i.name}"></div>
+        </div>
+        <div class="cardBody">
+          <div class="cardContent">
+            <h3 class="cardTitle">${i.name}</h3>
+            <p class="cardText">${i.description}</p>
+          </div>
+        </div>
+        <div class="cardFooter">
+          <div class="numberBlock">
+            <i class="fa-sharp fa-solid fa-circle-exclamation"></i>
+            <p>剩下最後 ${i.group} 組</p>
+          </div>
+          <div class="priceBlock">
+            <span>TWD</span>
+            <p class="price">$${i.price}</p>
+          </div>
+        </div>
+        </div>
+      </div>
+    </a>`;
+  })
+  
+  list.innerHTML = content;
+  filterResult.textContent = `共有 ${data.length} 筆資料，歡迎使用地區搜尋`;
+};
